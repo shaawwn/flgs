898113894a336924b0f88d5f20686cd7863c00de
Shawn Lentz <shawn.lentz@gmail.com>
2021-05-14 14:13:59 -1000

    Reworked front page

--- otherrealms/static/otherrealms/js/events.js
+++ otherrealms/static/otherrealms/js/events.js
@@ -1,13 +1,64 @@
-// Events scripts
+// Source for calender: https://www.youtube.com/watch?v=m9OSBJaQTlM
+let nav = 0; // Keeps track of month
+let clicked = null; // the day that has been clicked
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
-console.log("Loading events script...")
+const calendar = document.getElementById('calendar');
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
-document.addEventListener('DOMContentLoaded', () => {
+function load() {
+
+    const dt = new Date();
+
+    if (nav !== 0) {
+        dt.setMonth(new Date().getMonth() + nav);
+    }
     
-    ready()
+    const day = dt.getDate();
+    const month = dt.getMonth();
+    const year = dt.getFullYear()
 
-})
+    const firstDayOfMonth = new Date(year, month, 1);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-function ready() {
-    console.log("DOM loaded! Ready loaded!")
+    const dateString = firstDayOfMonth.toLocaleString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+      });
+
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
+    document.getElementById('monthDisplay').innerText = `${dt.toLocaleDateString('en-us', {month:'long'})} ${year}`;
+    
+    calendar.innerHTML = '';
+
+    for(let i = 1; i <= paddingDays + daysInMonth; i++){
+        daySquare = document.createElement('div');
+        daySquare.classList.add('day')
+
+        if (i > paddingDays) {
+            daySquare.innerText = i - paddingDays;
+            daySquare.addEventListener('click', () => console.log('click'));
+        } else {
+            daySquare.classList.add('padding');
+        }
+
+        calendar.appendChild(daySquare)
+    }
+
 }
+
+function initButtons() {
+    document.getElementById('nextButton').addEventListener('click', () => {
+        nav++;
+        load();
+    })
+    document.getElementById('backButton').addEventListener('click', () => {
+        nav--;
+        load();
+    })
+}
+
+initButtons();
+load()
