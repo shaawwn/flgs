6044b6d09f9dd78c1b92191870565bb08173f94d
Shawn Lentz <shawn.lentz@gmail.com>
2021-08-12 09:58:42 -1000

    Hid secret key maybe

--- flgs/flgs/settings.py
+++ flgs/flgs/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@)r$+lokj&iv69*2^oo))_ip&^j*f4&$0=-d77b_4*ytn9dkc*'
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
