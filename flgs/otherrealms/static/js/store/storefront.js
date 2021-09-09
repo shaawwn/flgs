814d76c866c2432a0e776937da260ede3880cdd4
Shawn Lentz <shawn.lentz@gmail.com>
2021-09-08 15:14:32 -1000

    added storefront app

---
+++ flgs/otherrealms/static/js/store/storefront.js
@@ -0,0 +1,7 @@
+console.log("Storefront scripts are loading....")
+
+// TEMP FUNCTIONS
+
+document.getElementsByClassName('header-image-container')[0].addEventListener('click', () => {
+    console.log("Redirecting...")
+})
